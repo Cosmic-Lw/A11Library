469f920c9339ee520579235cb4b4bafdef742705
npu2015303320 <2424733678@qq.com>
2017-07-19 19:20:16 +0800

    我的文档

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1> 注册 </h1>
+    <div> 用户名： <Input v-model="username" placeholder="请输入..." type = "text" style="width: 300px"></Input> </div>
+    <div> 密码：   <Input v-model="password" placeholder="请输入..." type = "password" style="width: 300px"></Input> </div>
+    <div> <Button type="primary" @click = "register"> 注册 </Button> </div>
+</div>
+</template>
+<script>
+
+export default{
+    data(){
+        return{
+            username: '123',
+            password: '123'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped>
+</style>
